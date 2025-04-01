fdce519205e68973d217c8b5fa35a8af288eacda
dagmtz <dagmtzs@gmail.com>
2025-03-31 23:44:53 -0600

    Loop even-odd

--- even-odd/src/main.rs
+++ even-odd/src/main.rs
@@ -1,21 +1,25 @@
-use std::io::{self, Write};
+use std::io::{self};
 fn main() {
 
-    let mut input_line = String::new();
+    let mut input_line = String::new(); 
+    
+    loop {
+        println!("Enter a number, I'll tell you whether it is \x1b[1meven \x1b[0mor \x1b[1modd\x1b[0m! ");
+        
+        io::stdin()
+            .read_line(&mut input_line)
+            .expect("Failed to read line");
 
-    print! ("Enter a number, I'll tell you whether it is \x1b[1meven \x1b[0mor \x1b[1modd\x1b[0m! ");
-    io::stdout().flush().unwrap();
-    
-    io::stdin()
-        .read_line(&mut input_line)
-        .expect("Failed to read line");
-    
-    
-    let x: i32 = input_line.trim().parse().expect("Input not an integer");
+        let x: i32 = input_line.trim().parse().expect("Input is not an integer! ");
 
-    if (x % 2) == 0 {
-        println!("{} is \x1b[1meven\x1b[0m!", x);      
-    } else {
-        println!("{} is \x1b[1modd\x1b[0m!", x);
-    }    
+        if x < 0 {
+            println!("I was expecting a positive number! Bye!");      
+            break;
+        } else if (x % 2) == 0 {
+            println!("{} is \x1b[1meven\x1b[0m!", x);      
+        } else {
+            println!("{} is \x1b[1modd\x1b[0m!", x);
+        }    
+    }
+    
 }
